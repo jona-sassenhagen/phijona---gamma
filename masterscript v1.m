2d6a34608a127e6be6ac89feed0a6aafd2e188e8
Jona <jona.sassenhagen@staff.uni-marburg.de>
2013-07-05 18:15:08 +0200

    update w/ extensive comments regarding what does and what doesn't work
    quite yet

--- masterscript v1.m
+++ masterscript v1.m
@@ -24,12 +24,14 @@ for X = 1:10
 end;
 
 
-
+clear max_t_coord
 % run massive list of ttests  
 % across experiments, frequencies, electrodes, time window lengths, time steps,
 % and save coordinates of max val
 
-winlength = [round(25/8),round(100/8),round(250/8)];
+% doesn't work yet b/c "allersp" actually has an individual name for each subj
+
+winlength = [round(25/8),round(100/8),round(250/8)];			% /8 because of 125 hz sampling rate
 
 for X = 1:10
 
@@ -48,17 +50,17 @@ for X = 1:10
 	[nfreqs,ntimes,nchans,nsubjs]=size(allersp);
 
 	coord=[];t=[];
-	for frequencies = 45:90
-		for e = 1:length(all_available_chans)
+	for frequencies = 45:90											% freqs(frequencies) will tell you what frequency it is
+		for e = 1:length(all_available_chans)						% chanlocs(elec).label will tell you what electrode it is
 			elec = electrode(e);									% choose elec (1/(length(C)))
 			for winsize = 1:length(winlength)						% choose timewins (1/4)
 				startingpositions = [0:winlength(winsize):round(1000/8)];
- 				for timewindow = 1:length(startingpositions)-1;
+ 				for timewindow = 1:length(startingpositions)-1;		% times(t1:t2) will tell you what time point you're looking at
 					windowstart = startingpositions(timewindow);
 																	% choose beginning of timewindow (depends on winsize)
 					for subject = 1:nsubjs							% choose subj (depends on exp)
 					
-						t1 = windowstart+61;
+						t1 = windowstart+61;						% 61 is time point 0 b/c the -760:1760 time window data is sampled at 125 hz
 						t2 = t1+(winlength(winsize));					
 						value(subject)=mean(allersp(frequencies,t1:t2,elec,subject));
 					end;
@@ -71,15 +73,15 @@ for X = 1:10
 		end;
 	end;
 	
-	[tvalue,max_tval_index] = max(abs(t));
-	max_t_coord{X} = coord(max_tval_index,:,:);
+	[tvalue,max_tval_index] = max(abs(t))							% find max val
+	max_t_coord{X} = [coord(max_tval_index,:,:)];					% save frequency/time/electrode coordinates for max val
+																	% there is one problem still .. elec is saved as a rather meaningless/experiment-specific number. it should probably be saved in reference to all_available_chans
 
-	
 
 end;
 
 
-
+% jackknife
 % calculate max val ttests again on each subject
 
 for X = 1:10
