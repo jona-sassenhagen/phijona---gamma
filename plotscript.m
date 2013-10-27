33c06a496bd97cbcff722f66b919edad14773790
Phillip Alday <phillip.alday@staff.uni-marburg.de>
2013-10-27 18:14:21 +0100

    slightly more structured programming

--- plotscript.m
+++ plotscript.m
@@ -1,8 +1,8 @@
 % masterplotscript
 
-
+num_exp = 11
 % find subset of common electrodes across all data sets
-for X = 1:11
+for X = 1:num_exp
 
 	sourcefile = ['/home/jona/gamma/',num2str((X),'%01i'),'c.mat'];
 	sourcefiles = ['/home/jona/gamma/',num2str(X),'s.mat'];
@@ -29,13 +29,13 @@ for X = 1:11
 	[R,F] = max(content_sizes);
 	allersp = eval(S(F).name);
 	clear S(F).name;											% ... just to clear it again
-	
+
 	clear labels;
 
 end;
 
 
-for X = 1:11
+for X = 1:num_exp
 
 	sourcefile = ['/home/jona/gamma/',num2str((X),'%01i'),'c.mat'];
 
@@ -69,7 +69,7 @@ for X = 1:11
 	A(1:50,:,:) = 0;
 	C = setdiff(electrode,1:nchans);
 	A(:,:,C) = 0;
-		
+
 	[maxval maxloc] = max(abs(A(:)));
 	[maxfreq maxtime maxchan] = ind2sub(size(A), maxloc);
 
@@ -84,7 +84,7 @@ for X = 1:11
 	A(20:end,:,:) = 0;
 	C = setdiff(electrode,1:nchans);
 	A(:,:,C) = 0;
-		
+
 	[maxval maxloc] = max(abs(A(:)));
 	[maxfreq maxtime maxchan] = ind2sub(size(A), maxloc);
 
@@ -100,7 +100,7 @@ for X = 1:11
 	A(1:3,:,:) = 0;
 	C = setdiff(electrode,1:nchans);
 	A(:,:,C) = 0;
-		
+
 	[maxval maxloc] = max(abs(A(:)));
 	[maxfreq maxtime maxchan] = ind2sub(size(A), maxloc);
 
@@ -146,11 +146,11 @@ lfc = ...
      1     1     0     0     0     0     0     0     0     1     1; ...
      0     0     1     1     0     1     1     0     0     0     0; ...
      1     1     1     1     1     0     1     0     0     0     0; ...
-     0     0     0     0     1     0     0     0     0     0     0; ... 
+     0     0     0     0     1     0     0     0     0     0     0; ...
      0     0     0     0     1     1     1     0     0     0     0; ...
      0     0     0     1     0     1     1     1     0     1     0; ...
      1     1     1     0     1     0     0     1     1     1     0; ...
-     1     1     1     1     0     1     1     1     1     1     0; ... 
+     1     1     1     1     0     1     1     1     1     1     0; ...
      1     1     1     0     1     1     1     1     1     1     0; ...
      1     0     1     0     0     0     0     0     0     1     1]
 
@@ -159,11 +159,11 @@ lfcc = ...
      1     0     0     0     0     0     0     0     0     1     1; ...
      0     0     0     1     0     1     1     0     0     0     0; ...
      1     1     1     0     1     0     1     0     0     0     0; ...
-     0     0     0     0     0     0     0     0     0     0     0; ... 
+     0     0     0     0     0     0     0     0     0     0     0; ...
      0     0     0     0     1     0     1     0     0     0     0; ...
      0     0     0     1     0     1     0     1     0     1     0; ...
      1     1     1     0     1     0     0     0     1     1     0; ...
-     1     1     1     1     0     1     1     1     0     1     0; ... 
+     1     1     1     1     0     1     1     1     0     1     0; ...
      1     1     1     0     1     1     1     1     1     0     0; ...
      1     0     1     0     0     0     0     0     0     1     0]
 
@@ -196,23 +196,23 @@ for Y = [   1     9    16    24    30   38    45    53    59    67    74]
 	tftopo(allersp,times,freqs,'mode','ave','limits', [nan nan nan nan -2.5 2.5], 'timefreqs', [times(all_max_coordinates_gamma(X,2)) freqs(all_max_coordinates_gamma(X,1));  times(all_max_coordinates_theta(X,2)) freqs(all_max_coordinates_theta(X,1)); times(all_max_coordinates_alpha(X,2)) freqs(all_max_coordinates_alpha(X,1));], 'chanlocs', chanlocs,'smooth',1.5,'style','map','electrodes','off','cbar','off');
 
 	sbplot(3,29,Y+5);
-	bar([10 10],'FaceColor','w','EdgeColor','w')
+	bar([num_exp num_exp],'FaceColor','w','EdgeColor','w')
 	hold on;
 
-	bar([mean(lfc(X,1:10))*10 0],'FaceColor',[0.5,0.5,0.9],'EdgeColor','b')
+	bar([mean(lfc(X,1:num_exp))*num_exp 0],'FaceColor',[0.5,0.5,0.9],'EdgeColor','b')
 	hold on;
-	bar([mean(lfcc(X,1:10))*10 0],'b')
+	bar([mean(lfcc(X,1:num_exp))*num_exp 0],'b')
 	hold on;
-	bar([0 mean(gammac(X,1:10))*10],'FaceColor',[0.9,0.5,0.5],'EdgeColor','r')
+	bar([0 mean(gammac(X,1:num_exp))*num_exp],'FaceColor',[0.9,0.5,0.5],'EdgeColor','r')
 	hold on;
-	bar([0 mean(gammacc(X,1:10))*10],'r')
+	bar([0 mean(gammacc(X,1:num_exp))*num_exp],'r')
 
 
 	%ylabel(gca,'Alignment')
 	set(gca,'XTick',[1 2])
 	set(gca,'XTickLabel',{'<12 hz','>30 hz'})
-	set(gca,'YTick',[5 10])
-	set(gca,'YTickLabel',{'5','10'})
+	set(gca,'YTick',[5 num_exp])
+	set(gca,'YTickLabel',{'5',num2str(num_exp)})
 	ylabel('')
 	X = X+1;
 %	color = get(h,'Color');
